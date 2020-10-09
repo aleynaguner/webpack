2914e080a64ef139b2051b614480bc9989b2a7d7
Aleyna Güner <aleynaguner@hotmail.com>
2020-10-09 13:16:20 +0300

    Fix Conflict

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDom from "react-dom";
 
@@ -7,24 +6,8 @@ import "./style.scss";
 const title = "HELLO WEBPACK";
 
 ReactDom.render(
-    <div className={"title"}>{title}</div>,
-    document.getElementById("app")
-)
+  <div className={"title"}>{title}</div>,
+  document.getElementById("app")
+);
 
 module.hot.accept();
-=======
-import React from "react";
-import ReactDom from "react-dom";
-
-import "./style.scss";
-
-const title = "HELLO WEBPACK";
-
-ReactDom.render(
-    <div className={"title"}>{title}</div>,
-    document.getElementById("app")
-)
-
-module.hot.accept();
->>>>>>> 6b867ff6f4c6d3ed7039976dbdf1f248c1594b3f
-//

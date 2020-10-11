f59d4aabef3333aa1d90e97b8e98cef0fc06fa20
Aleyna Güner <aleynaguner@hotmail.com>
2020-10-11 16:19:37 +0300

    Fix Conflict

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 module.exports={
     plugins: [
         require("autoprefixer")
     ]
-=======
-module.exports={
-    plugins: [
-        require("autoprefixer")
-    ]
->>>>>>> 6b867ff6f4c6d3ed7039976dbdf1f248c1594b3f
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const webpack = require("webpack");
 
 module.exports={
@@ -28,35 +27,4 @@ module.exports={
     plugins:[
         new webpack.HotModuleReplacementPlugin()
     ]
-=======
-const webpack = require("webpack");
-
-module.exports={
-    entry: "./src/index.js",
-    output:{
-        path: __dirname + "/dist",
-        publicPath: "/",
-        filename:"bundle.js"
-    },
-    devServer: {
-        contentBase: "./dist",
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|es6|jsx)$/,
-                exclude: /node_modules/, //bu dosyanın içinde böyle bir dosya bulmasına gerek yok
-                use: ["babel-loader"]
-            },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
-            }
-        ]
-    },
-    plugins:[
-        new webpack.HotModuleReplacementPlugin()
-    ]
->>>>>>> 6b867ff6f4c6d3ed7039976dbdf1f248c1594b3f
 }
